4ebc930bfa2a78c57fc52e37fedd9eee9cc5a176
RedwoodTechno <148137100+RedwoodTechno@users.noreply.github.com>
2023-10-17 01:01:58 +0530

    Update Sample.java

--- src/com/simple/Sample.java
+++ src/com/simple/Sample.java
@@ -4,7 +4,7 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Sample...");
+		System.out.println("Hello Sample..cxcxcx.");
 
 	}
 
